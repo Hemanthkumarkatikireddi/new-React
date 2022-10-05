9fe3a773dcceb75e01f079699e0a4226b9aa309f
Hemanth Kumar <khemanthkumar360@gmail.com>
2022-10-05 15:27:35 +0000

    congrats card

---
+++ congratsCardApp/index.js
@@ -0,0 +1,25 @@
+const element = (
+  // Write your code here.
+  <div class="main-container">
+    <div class="cd-1">
+      <h1 class="heading">Congratulations</h1>
+    </div>
+
+    <div class="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        class="img"
+      />
+      <h1 class="name">Kiran V</h1>
+      <p class="discription">
+        Vishnu institute of Computer Education and Technology, Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="sub-img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
---
+++ superOverLeagueApp/index.js
@@ -0,0 +1,18 @@
+const element = (
+  //   Write your code here.
+  <div className="main-container">
+    <h1 class="heading">Super Over League</h1>
+    <div class="img-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="logo"
+      />
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="logo"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
